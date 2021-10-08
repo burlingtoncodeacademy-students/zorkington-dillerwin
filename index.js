04118eb0360454ed2f1a7bec449f68edfde107ea
dillerwin <dillerwin@gmail.com>
2021-10-08 10:52:46 -0400

    Have some objects set up, rooms 1-3 done

--- index.js
+++ index.js
@@ -20,18 +20,32 @@ function ask(questionText) {
   });
 }
 
-// beginning of actual program
+// defining Room class
+class Room {
+  constructor(name, desc = "", exits = [], inventory = [], examine = "") {
+    this.name = name; //name of room
+    this.desc = desc; // description of room
+    this.exits = exits; // exits from this room
+    this.inventory = inventory;
+    this.examine = examine;
+  }
+}
 
+//defining interactable object class
 class useableThing {
   //we're gonna have things. Name, description
-  constructor(name, desc) {
+  constructor(name, desc, use) {
     this.name = name;
     this.desc = desc;
+    this.use = use;
   }
-  read() {
+  examine() {
     console.log(this.desc);
   }
-  pickup() {
+  use() {
+    console.log(`You use ${this.name}.`);
+  }
+  take() {
     console.log(`You pick up ${this.name}.`);
   }
   drop() {
@@ -39,47 +53,104 @@ class useableThing {
   }
 }
 
-class Room {
-  constructor(name, desc = "", exits = []) {
+// things to add: flashlight, backpack, door, wooden stalls
+
+// defining uninteractable objects
+class unusableThing {
+  constructor(name, desc, examine) {
     this.name = name;
     this.desc = desc;
-    this.exits = exits;
+    this.examine = examine;
+  }
+  interact() {
+    console.log(`There's nothing you can do with this object.`);
+  }
+  inspect() {
+    console.log(this.examine);
   }
 }
 
+//defining unusable items
+let puddle = new unusableThing(
+  `large puddle`,
+  `This is a large pool of water.`,
+  wrap(
+    `Looking closer, this puddle probably leaked through the roof and floors above when it rained earlier this week. It's not very deep.`,
+    60
+  )
+);
+
+let oldLine = new unusableThing(
+  `old textile line`,
+  wrap(
+    `this machine probably had more to it at one point, but at this point it's basically just an odd table.`,
+    60
+  )
+);
+
+// defining rooms
 let roomOne = new Room( //entrance room at bottom left of map
-  `Room One`,
-  undefined
+  `outside`,
+  `You stand outside the old factory. It looks the way you expected, run down and abandoned.`,
+  [`Inside`],
+  [`metal pole`],
+  wrap(
+    `You look around the outside of the building. There's a smaller building off to the side you've been told to stay out of, the floor isn't safe. You also find an old metal pole about three feet long.`,
+    60
+  )
 );
 
 let roomTwo = new Room( //
-  `Room Two`,
-  `You enter the old factory. You find yourself in a long room that spans the length of the building, taking up most this floor.`
+  `Floor One Main Room`,
+  wrap(
+    `You enter the old factory. You find yourself in a long room that spans the length of the building, taking up most this floor. Structural supports dot the area. You can see a large puddle by the southern wall, some wooden stalls by the northern wall, a brick structure in the center of the room that runs up into the ceiling, and you can see a door to a stairwell in the northwest corner. The back of the room fades into shadow. `,
+    60
+  ),
+  [`northwest stairwell`, `southeast stairwell`, `outside`],
+  [`roof key`, `flashlight`],
+  wrap(
+    `You walk a loop around the bottom floor. The stalls appear to be plywood, set up by someone after the factory closed. They're covered in graffiti. The brick structure in the middle of the room is an old elevator lift shaft. The back wall has a large freight door, it's chained shut. There is a second stairwell in the southeast corner.`,
+    60
+  )
 );
 
-// console.log(roomTwo.desc());
-
 let roomThree = new Room( // next room in building after room 1. Not sure what will be in here yet
-  `Room Three`,
-  `A larger room. This room has doors in the west and north walls.`
+  `Northwest Stairwell`,
+  wrap(
+    `This stairwell is in disrepair. A few steps are missing, but none that make it impossible to use.`,
+    60
+  ),
+  [`Main floor`, `Floor 2`],
+  [],
+  wrap(
+    `You look around the stairwell carefully, but there's nothing special to be found here.`,
+    60
+  )
 );
 
 let roomFour = new Room( // room after room 3. Not sure what is in here yet. Has hidden passage to room 7.
-  `Room Four`,
+  `South Stairwell`,
   `Another large room. This room is well lit.`
 );
 
 let roomFive = new Room( // room after room 4. Has doors to rooms 4, 6, and 8. Room 8 is a secret room behind a bookshelf
-  `Room Five`,
+  `Floor Two Main Room`,
   `a smaller room`
 );
 
 let roomSix = new Room( // final room. Not sure what will be here. Boss fight of some kind? Secret passage to/from room 7 behind a tapestry
-  `Room Six`,
+  `Roof`,
   `A large, well lit room. There is a suspicious amount of health and ammo by the door.`
 );
 
-let transitions = {};
+let transitions = {
+  roomOne: [`room two`],
+  roomTwo: [`room one`],
+  roomThree: [],
+  roomFour: [],
+  roomFive: [],
+  roomSix: [],
+};
 
 // function changeRoom(newRoom) { //moves you from room to room
 //   // move between rooms in game
@@ -88,10 +159,10 @@ let transitions = {};
 // }
 
 async function start() {
-  const welcomeMessage = `Hello`
-  welcomeMessage = await ask(`Welcome! This text adventure is about Urban Exploration!\nYou'll be exploring an old textile factory just outside of town.`);
-  let answer = await ask(welcomeMessage);
+  const welcomeMessage = `Welcome! This text adventure is about Urban Exploration!\nYou'll be exploring an old textile factory just outside of town.\nTo interact with the world in the game, type them right into the cmd line.\nWe'll be using 'take', 'drop', 'examine', 'move',\nand a few others to be decided later.`;
+  console.log(welcomeMessage);
+  let message = await ask(`Are you read to begin your exploration? `);
   // process.exit();
 }
 
-start();
+// start();
